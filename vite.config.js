9eabf89f2ebe5a54ad27925e1132db11d658510a
Maxwellcoyle-dev <maxwellc.henderson@gmail.com>
2023-10-16 15:25:27 -0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
+  esbuild: {
+    loader: "jsx",
   },
   resolve: {
-    alias: [
-      {
-        find: "./runtimeConfig",
-        replacement: "./runtimeConfig.browser", // ensures browser compatible version of AWS JS SDK is used
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
       },
-    ],
+    },
   },
 });
